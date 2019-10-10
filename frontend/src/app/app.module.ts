8bc620a48cbc75ebca4582a1844ce85c9dbffb8e
AnTran1997 <1512012@student.hcmus.edu.vn>
2019-10-10 17:45:19 +0700

    Add single-product component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,19 +7,22 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SingleProductComponent } from './single-product/single-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SingleProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent}
+      {path: '', component: HomeComponent},
+      {path: 'single-product', component: SingleProductComponent}
     ])
   ],
   providers: [],
